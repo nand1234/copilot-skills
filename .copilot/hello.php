df5234f78f96ef93ee2068cccea63828ee0197b5
nandkumar ghatage <nandkumar.ghatge@smartbox.com>
2026-02-03 11:47:50 +0000

    added skills.md filr to the repo

---
+++ .copilot/hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+// This is a single-line comment in PHP
+echo "Hello World!"; // The echo statement is used to output data
+?>
+
+</body>
+</html>
